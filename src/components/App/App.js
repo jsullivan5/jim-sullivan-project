596d0872ad54d1a0522a192b3e64de49c46c0ab8
James Sullivan <jamesfsullivan5@gmail.com>
2021-09-04 12:58:40 -0600

    Add material UI

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import AppBar, { Fragment } from '../AppBar/AppBar';
+import './App.css';
+
+function App() {
+  return (
+		<Fragment>
+    	<AppBar />
+		</Fragment>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+		<ThemeProvider theme={theme}>
+    	<CssBaseline />
+    	<App />
+		</ThemeProvider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
